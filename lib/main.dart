bde57ccf7980838004907596cfc85814537ddc9d
yavuzfk <yavuzfk@hotmail.com>
2022-12-27 14:39:08 +0300

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:features/hive_tools.dart';
-import 'package:features/page_design/page_design_view.dart';
+import 'package:features/page_design/design_splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -16,6 +17,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
       // home: hiveRead(HiveGetText().hiveABoxKey)
       //     ? const HomePage()
       //     : const OnBoardingScreen(),
-      home: const PageDesign(),
+      home: const DesignSplash(),
     );
   }
 }
---
+++ lib/page_design/design_splash.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DesignSplash extends StatefulWidget {
+  const DesignSplash({Key? key}) : super(key: key);
+
+  @override
+  _DesignSplashState createState() => _DesignSplashState();
+}
+
+class _DesignSplashState extends State<DesignSplash> {
+  // final String assetName = 'assets/yuvarlak_opak.svg';
+  final Widget svgCircle = SvgPicture.asset('assets/yuvarlak_opak.svg');
+
+  double diameterValue(BuildContext context) =>
+      MediaQuery.of(context).size.width * 1.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEEEEE),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 64, 59, 59), Colors.green],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight)),
+        child: Stack(
+          children: [
+            Positioned(top: diameterValue(context) / 3.2, child: svgCircle),
+            Positioned(
+              top: diameterValue(context) / 2.2,
+              right: diameterValue(context) / 110,
+              child: svgCircle,
+            ),
+            Center(
+              child: SizedBox(
+                  width: 160,
+                  height: 160,
+                  child: Image.asset('assets/logo_romsis.png')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
