8ce72bd0f53c4fe8bb7abf53a0afd29d101a3e48
yavuzfk <yavuzfk@hotmail.com>
2023-01-24 11:55:51 +0300

    card designed

---
+++ lib/card_design/card_design.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CardDesign extends StatefulWidget {
+  const CardDesign({Key? key}) : super(key: key);
+
+  @override
+  _CardDesignState createState() => _CardDesignState();
+}
+
+class _CardDesignState extends State<CardDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: ),
+    );
+  }
+}
